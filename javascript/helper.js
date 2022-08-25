bc550a95e2053fbd9c72c82c50445c9f54b491c2
JovialBadger <68871905+JovialBadger@users.noreply.github.com>
2022-08-25 17:31:28 +0100

    wordFinderDaily

--- javascript/helper.js
+++ javascript/helper.js
@@ -172,3 +172,62 @@ function setLocal(id, value){
 function delLocal(id){
 	localStorage.removeItem(id);
 }
+
+function cyrb128(str) {
+    let h1 = 1779033703, h2 = 3144134277,
+        h3 = 1013904242, h4 = 2773480762;
+    for (let i = 0, k; i < str.length; i++) {
+        k = str.charCodeAt(i);
+        h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+        h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+        h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+        h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+    }
+    h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+    h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+    h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+    h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+    return [(h1^h2^h3^h4)>>>0, (h2^h1)>>>0, (h3^h1)>>>0, (h4^h1)>>>0];
+}
+
+function randSeed(seed){	
+	// Create cyrb128 state:
+	var seed = cyrb128(seed);
+	// Four 32-bit component hashes provide the seed for sfc32.
+	var rand = sfc32(seed[0], seed[1], seed[2], seed[3]);
+	return rand();
+}
+
+function itemFromObjDateSeed(obj){
+	if(obj.length > 0){
+		var objNumber = Math.floor(randSeed(UTCString()) * (obj.length + 1));
+		return obj[objNumber];
+	}
+}
+
+function sfc32(a, b, c, d) {
+    return function() {
+      a >>>= 0; b >>>= 0; c >>>= 0; d >>>= 0; 
+      var t = (a + b) | 0;
+      a = b ^ b >>> 9;
+      b = c + (c << 3) | 0;
+      c = (c << 21 | c >>> 11);
+      d = d + 1 | 0;
+      t = t + d | 0;
+      c = c + t | 0;
+      return (t >>> 0) / 4294967296;
+    }
+}
+
+function UTCString(){
+	const date = new Date();
+	return (date.getUTCFullYear()+"-"+addLeadingZeros(date.getUTCMonth()+1,2)+"-"+addLeadingZeros(date.getUTCDate(),2));
+}
+
+function addLeadingZeros(num, totalLength) {
+  if (num < 0) {
+    const withoutMinus = String(num).slice(1);
+    return '-' + withoutMinus.padStart(totalLength, '0');
+  }
+  return String(num).padStart(totalLength, '0');
+}
