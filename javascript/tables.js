622b231d4c2ccf7572d6662ebf3a4b6210a14d24
JovialBadger <68871905+JovialBadger@users.noreply.github.com>
2023-07-08 14:22:35 +0100

    Update tables.js

--- javascript/tables.js
+++ javascript/tables.js
@@ -135,9 +135,8 @@ function filterTable(tableid, columnid, search, matchType) {//update to use loca
         }
     }
 	setLocal("localTable" + tableid, JSON.stringify(table));
-	var displayTable = table.splice(0, 1);
-	displayTable = displayTable.filter(a => getContainerVal(a,containerRow) === 0 ? false : true);
-	//displayTable = displayTable.filter(b => getContainerVal(b,containerCell) === 0 ? flase : true);
+	table.splice(0, 1);
+	var displayTable = table.filter(a => getContainerVal(a,containerRow) === 0 ? false : a.filter(b => getContainerVal(b,containerCell) === 0 ? flase : true));
 	getTags("tbody", getElem(tableid))[0].outerHTML = createTableBody(displayTable);
 }
 
@@ -273,7 +272,8 @@ function sortTable(tableid, col, sortdir) {
     // }
 	//updateTableStriping(table);
 	setLocal("localTable" + tableid, JSON.stringify(table));
-	var displayTable = table.splice(0, 1).filter(a => getContainerVal(a,containerRow) === 0 ? false : a.filter(b => getContainerVal(b,containerCell) === 0 ? flase : true));
+	table.splice(0, 1);
+	var displayTable = table.filter(a => getContainerVal(a,containerRow) === 0 ? false : a.filter(b => getContainerVal(b,containerCell) === 0 ? flase : true));
 	getTags("tbody", getElem(tableid))[0].outerHTML = createTableBody(displayTable);
 }
 
